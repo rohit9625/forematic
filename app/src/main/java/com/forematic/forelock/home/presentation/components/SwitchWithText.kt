410bb8fd8ebf46e6b19f5cf9773f78938005b403
Rohit Verma <rv17837@outlook.com>
2024-12-18 23:28:52 +0530

    UI: add custom switch component

---
+++ app/src/main/java/com/forematic/forelock/home/presentation/components/SwitchWithText.kt
@@ -0,0 +1,91 @@
+package com.forematic.forelock.home.presentation.components
+
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.forematic.forelock.ui.theme.ForeLockTheme
+
+@Composable
+fun SwitchWithText(
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    textOnOFF: Pair<String, String> = Pair("ON", "OFF")
+) {
+    val switchWidth = 80.dp
+    val switchHeight = 32.dp
+    val thumbSize = 24.dp
+
+    val backgroundColor = if (isChecked) Color(0xFF4CAF50) else Color.Red // Green or Gray
+    val textColor = Color.White
+
+    val thumbOffset by animateDpAsState(
+        targetValue = if (isChecked) switchWidth - thumbSize - 6.dp else 6.dp,
+        label = "Thumb Offset Animation"
+    )
+
+    Box(
+        modifier = modifier
+            .size(width = switchWidth, height = switchHeight)
+            .clip(RoundedCornerShape(50))
+            .background(backgroundColor)
+            .clickable { onCheckedChange(!isChecked) }
+    ) {
+        Text(
+            text = if (isChecked) textOnOFF.first else textOnOFF.second,
+            color = textColor,
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier
+                .padding(horizontal = 6.dp)
+                .align(if (isChecked) Alignment.CenterStart else Alignment.CenterEnd)
+        )
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .size(thumbSize)
+                .offset {
+                    IntOffset(x = thumbOffset.roundToPx(), y = 0)
+                }
+                .clip(CircleShape)
+                .background(Color.White)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SwitchWithTextPreview() {
+    var isSwitchOn by remember { mutableStateOf(false) }
+
+    ForeLockTheme {
+        Surface {
+            SwitchWithText(
+                isChecked = isSwitchOn,
+                onCheckedChange = { isSwitchOn = it },
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
