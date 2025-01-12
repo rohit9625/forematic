63b50062897253f60bb18c18a9ddd8a18ed7bb95
Rohit Verma <rv17837@outlook.com>
2025-01-12 19:16:33 +0530

    add some input parameters and refactor code

--- app/src/main/java/com/forematic/forelock/setupdevice/presentation/components/TextFields.kt
+++ app/src/main/java/com/forematic/forelock/setupdevice/presentation/components/TextFields.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
@@ -16,16 +15,15 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
 import com.forematic.forelock.ui.theme.ForeLockTheme
 
 @Composable
@@ -35,20 +33,25 @@ fun LabeledTextField(
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
     secondaryLabel: String? = null,
-    placeholder: @Composable (() -> Unit)? = null,
+    isEnabled: Boolean = true,
+    placeholder: String? = null,
     trailingIcon: @Composable (() -> Unit)? = null,
     keyboardActions: KeyboardActions = KeyboardActions.Default,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     textStyle: TextStyle = MaterialTheme.typography.bodyMedium,
-    shape: Shape = RoundedCornerShape(12.dp)
+    shape: Shape = MaterialTheme.shapes.medium,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    focusRequester: FocusRequester = FocusRequester()
 ) {
     Column(
         modifier = modifier.width(IntrinsicSize.Max),
-        verticalArrangement = Arrangement.spacedBy(4.dp)
+        verticalArrangement = Arrangement.spacedBy(2.dp)
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 4.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp)
         ) {
             Text(
                 text = label,
@@ -66,10 +69,21 @@ fun LabeledTextField(
         OutlinedTextField(
             value = value,
             onValueChange = onValueChange,
-            modifier = Modifier.height(50.dp),
-            placeholder = placeholder,
+            modifier = Modifier.height(50.dp)
+                .focusRequester(focusRequester = focusRequester),
+            placeholder = {
+                placeholder?.let {
+                    Text(
+                        text = it,
+                        style = textStyle,
+                        color = Color.Gray
+                    )
+                }
+            },
+            visualTransformation = visualTransformation,
             textStyle = textStyle,
             shape = shape,
+            enabled = isEnabled,
             singleLine = true,
             trailingIcon = trailingIcon,
             keyboardActions = keyboardActions,
@@ -80,28 +94,14 @@ fun LabeledTextField(
 
 @Preview
 @Composable
-private fun ToolTipPreview() {
-    ForeLockTheme { 
-        Surface {
-            Popup {
-                Text(text = "This is a tool tip popup.")
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
 private fun LabeledTextFieldPreview() {
-    var text by remember { mutableStateOf("") }
     ForeLockTheme {
         Surface {
             LabeledTextField(
-                value = text,
-                onValueChange = { text = it },
+                value = "",
+                onValueChange = { },
                 label = "Label Preview",
-                modifier = Modifier.padding(16.dp),
-                shape = RoundedCornerShape(bottomStart = 12.dp, bottomEnd = 12.dp)
+                modifier = Modifier.padding(16.dp)
             )
         }
     }
