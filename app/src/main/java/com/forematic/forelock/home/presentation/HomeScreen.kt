1fd419ab31592b2b03539d2d57f6e1a362145e96
Rohit Verma <rv17837@outlook.com>
2025-01-19 22:25:33 +0530

    add home screen and setup navigation

--- app/src/main/java/com/forematic/forelock/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/forematic/forelock/home/presentation/HomeScreen.kt
@@ -1,18 +1,28 @@
 package com.forematic.forelock.home.presentation
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,6 +31,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -32,6 +43,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -48,6 +60,9 @@ import androidx.navigation.compose.rememberNavController
 import com.forematic.forelock.R
 import com.forematic.forelock.Route
 import com.forematic.forelock.home.presentation.components.ExpandableFAB
+import com.forematic.forelock.home.presentation.components.SwitchWithText
+import com.forematic.forelock.setupdevice.presentation.DeviceType
+import com.forematic.forelock.setupdevice.presentation.OutputRelayText
 import com.forematic.forelock.ui.theme.ForeLockTheme
 import kotlinx.coroutines.launch
 
@@ -70,6 +85,9 @@ fun HomeScreen(
             "G24 Intercom" to { navController.navigate(Route.G24SetupScreen) }
         ))
     }
+
+    var isBigGateOpen by remember { mutableStateOf(false) }
+    var isOfficeGateOpen by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
@@ -104,23 +122,27 @@ fun HomeScreen(
                 .padding(innerPadding)
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Button(
-                onClick = {
-                    if (hasPermission()) {
-                        onEvent(HomeScreenEvent.UpdateDialogVisibility(true))
-                    } else {
-                        requestPermission()
-                    }
-                },
-                shape = RoundedCornerShape(12.dp)
-            ) {
-                Text(
-                    text = "Open".uppercase(),
-                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp)
-                )
-            }
+            GateDeviceControls(
+                gateName = "Office Gate",
+                isGateOpen = isOfficeGateOpen,
+                gateIcon = R.drawable.ic_office_gate_brown,
+                deviceType = DeviceType.G24_INTERCOM,
+                statusText = OutputRelayText.UNLOCK_LOCK,
+                onStatusChange = { isOfficeGateOpen = it },
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+
+            GateDeviceControls(
+                gateName = "Big Gate",
+                isGateOpen = isBigGateOpen,
+                gateIcon = R.drawable.ic_big_gate_brown,
+                deviceType = DeviceType.G24_INTERCOM,
+                statusText = OutputRelayText.OPEN_CLOSE,
+                onStatusChange = { isBigGateOpen = it },
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
         }
     }
 
@@ -243,6 +265,114 @@ fun OpenGateDialog(
                     modifier = Modifier.size(28.dp)
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun GateDeviceControls(
+    gateName: String,
+    isGateOpen: Boolean,
+    @DrawableRes gateIcon: Int,
+    deviceType: DeviceType,
+    statusText: OutputRelayText,
+    onStatusChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val splitStatusText = remember(statusText) { splitStatusText(statusText.displayName) }
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(24.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+                .fillMaxWidth()
+                .height(IntrinsicSize.Max)
+        ) {
+            Icon(
+                painter = painterResource(gateIcon),
+                contentDescription = gateName,
+                tint = Color.Unspecified,
+                modifier = Modifier.size(48.dp)
+            )
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Text(
+                    text = gateName,
+                    style = MaterialTheme.typography.titleMedium.copy(fontSize = 18.sp),
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = deviceType.displayName,
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                SwitchWithText(
+                    isChecked = isGateOpen,
+                    onCheckedChange = onStatusChange,
+                    textOnOFF = splitStatusText
+                )
+            }
+
+            ElevatedButton(
+                onClick = { },
+                contentPadding = PaddingValues(),
+                shape = CircleShape,
+                modifier = Modifier.size(48.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Call,
+                    contentDescription = null,
+                    tint = Color.Blue
+                )
+            }
+        }
+    }
+}
+
+/**
+ * Splits the input string into two parts based on the "/" delimiter.
+ *
+ * @param text The input string to be split, expected to contain a single "/".
+ *             For example: "Open/Close".
+ * @return A Pair where the first element is the substring before the "/",
+ *         and the second element is the substring after the "/".
+ */
+fun splitStatusText(text: String): Pair<String, String> {
+    val first = text.substringBefore('/')
+    val second = text.substringAfter('/')
+    return Pair(first, second)
+}
+
+
+@Preview
+@Composable
+private fun DeviceItemPreview() {
+    ForeLockTheme {
+        Surface {
+            GateDeviceControls(
+                gateName = "Office Gate",
+                isGateOpen = false,
+                gateIcon = R.drawable.ic_office_gate_brown,
+                deviceType = DeviceType.G24_INTERCOM,
+                statusText = OutputRelayText.OPEN_CLOSE,
+                onStatusChange = { },
+                modifier = Modifier.padding(16.dp)
+                    .fillMaxWidth()
+            )
         }
     }
 }
