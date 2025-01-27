72cedeaacb901156f2fb9f6fb2b29d98bf96d909
Rohit Verma <rv17837@outlook.com>
2025-01-27 12:53:47 +0530

    refactor regex for programming password

--- app/src/main/java/com/forematic/forelock/core/domain/InputValidator.kt
+++ app/src/main/java/com/forematic/forelock/core/domain/InputValidator.kt
@@ -14,7 +14,7 @@ class InputValidator {
     }
 
     fun validateProgrammingPassword(password: String): Result<Unit, InputError.PasswordError> {
-        val regex = Regex("^([A-Z]{4}|[0-9]{4})$")
+        val regex = Regex("^[A-Z0-9]{4}\$")
         return if(password.length != 4) {
             return Result.Failure(InputError.PasswordError.INVALID_LENGTH)
         } else if(!regex.matches(password)) {
